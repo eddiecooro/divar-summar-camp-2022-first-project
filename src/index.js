203782087743ff5371b3bc1af4098108ff1723e0
Milad Rabizadeh <milad@Milads-MacBook-Air.local>
2022-08-01 16:50:04 +0430

    Sperating onclick function

--- src/index.js
+++ src/index.js
@@ -6,16 +6,24 @@ import './styles/index.scss';
 
 // To be imported
 
-function onClick(e) {
+function cat_onClick(e) {
     e.stopPropagation();
     const menu = document.querySelector('section.megamenu');
     const container = document.querySelector('div#overlay');
     menu.classList.toggle('megamenu-clicked');
     container.classList.toggle('not-clickable');
 }
+
+function button_onClick(e) {
+    e.stopPropagation();
+    const menu = document.querySelector('section.megamenu');
+    const container = document.querySelector('div#overlay');
+    container.classList.remove('not-clickable');
+    menu.classList.remove('megamenu-clicked');
+}
 window.onload = () => {
     const megamenu_button = document.querySelector('button.megamenu-button');
-    megamenu_button.onclick = onClick;
-    document.onclick = onClick;
+    megamenu_button.onclick = cat_onClick;
+    document.onclick = button_onClick;
 }
 
