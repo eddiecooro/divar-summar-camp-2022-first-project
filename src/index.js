9a2d101171bc39aeabf9939b4869ba099cb64f8e
Milad Rabizadeh <milad@Milads-MacBook-Air.local>
2022-08-01 16:33:49 +0430

    Feature: onClick for megamenu and overlays

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './reset.css';
 import './fonts/stylesheet.css';
 import './styles/index.scss';
+
+
+
+// To be imported
+
+function onClick(e) {
+    e.stopPropagation();
+    const menu = document.querySelector('section.megamenu');
+    const container = document.querySelector('div#overlay');
+    menu.classList.toggle('megamenu-clicked');
+    container.classList.toggle('not-clickable');
+}
+window.onload = () => {
+    const megamenu_button = document.querySelector('button.megamenu-button');
+    megamenu_button.onclick = onClick;
+    document.onclick = onClick;
+}
+
