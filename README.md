fb6a2b89b58be2821458a59dfb3532b215b79969
Eddie CooRo <eddiecooro@gmail.com>
2022-08-01 14:28:37 +0430

    Init new

--- README.md
+++
@@ -1,1 +0,0 @@
-You can view the results at https://eddie-cooro.github.io/divar-summar-camp-2022-first-project/
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'source-map',
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].svg',
+              outputPath: 'icons',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin(), new HtmlWebpackPlugin()],
+};
