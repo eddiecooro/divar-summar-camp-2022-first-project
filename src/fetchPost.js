f60cf2a9f12af3f6fc8b31b91726a0d24938cb42
mpowend <mahdi.pourtalebi@gmail.com>
2022-08-03 12:56:53 +0430

    image carousel working

--- src/fetchPost.js
+++ src/fetchPost.js
@@ -1,4 +1,4 @@
-import {deepSearchByKey,deepSearchByPair} from "./utils.js"
+import { deepSearchByKey, deepSearchByPair } from "./utils.js";
 let url = window.location.href;
 if (url.includes("post.html")) {
   let url_split = url.split("=");
@@ -25,35 +25,49 @@ if (url.includes("post.html")) {
   })
     .then((r) => r.json())
     .then((b) => {
-      console.log(b)
-      let data = b.sections
-      let title = deepSearchByKey(data, "title")["title"]
-      let author = deepSearchByKey(data, "subtitle")["subtitle"]
-      let chips = deepSearchByKey(data, "chips")["chips"]
-      let infobox = null
+      console.log(b);
+      let data = b.sections;
+      let title = deepSearchByKey(data, "title")["title"];
+      let author = deepSearchByKey(data, "subtitle")["subtitle"];
+      let chips = deepSearchByKey(data, "chips")["chips"];
+      let infobox = null;
       try {
-        infobox = deepSearchByPair(data, "widget_type", "GROUP_INFO_ROW")[0]["data"]["items"]
-      } catch (error) {
-      }
+        infobox = deepSearchByPair(data, "widget_type", "GROUP_INFO_ROW")[0][
+          "data"
+        ]["items"];
+      } catch (error) {}
 
-      let rows = deepSearchByPair(data, "widget_type","UNEXPANDABLE_ROW")
-      let description = deepSearchByPair(data, "widget_type","DESCRIPTION_ROW")[0]["data"]["text"]
-      description=description.replaceAll("\n","<br/>")
+      let rows = deepSearchByPair(data, "widget_type", "UNEXPANDABLE_ROW");
+      let description = deepSearchByPair(
+        data,
+        "widget_type",
+        "DESCRIPTION_ROW"
+      )[0]["data"]["text"];
+      description = description.replaceAll("\n", "<br/>");
+      let images = deepSearchByPair(data, "widget_type", "IMAGE_CAROUSEL")[0][
+        "data"
+      ]["items"];
 
-      console.log(description);
+      console.log(images);
 
-      let container = document.querySelector(".single-post-container")
+      let container = document.querySelector(".single-post-container");
       container.innerHTML = `
       <main class="single-post">
         <nav class="single-post__breadcrumb">
           <span class="single-post__arrowed-text">
-            املاک <img class="single-post__arrow"src="${require("./icons/svgs/chevron-left.svg" ).default}"/>
+            املاک <img class="single-post__arrow"src="${
+              require("./icons/svgs/chevron-left.svg").default
+            }"/>
           </span>
           <span class="single-post__arrowed-text">
-            فروش مسکونی <img class="single-post__arrow"src="${require("./icons/svgs/chevron-left.svg" ).default}"/>
+            فروش مسکونی <img class="single-post__arrow"src="${
+              require("./icons/svgs/chevron-left.svg").default
+            }"/>
           </span>
           <span class="single-post__arrowed-text">
-            آپارتمان <img class="single-post__arrow"src="${require("./icons/svgs/chevron-left.svg" ).default}"/>
+            آپارتمان <img class="single-post__arrow"src="${
+              require("./icons/svgs/chevron-left.svg").default
+            }"/>
           </span>
         </nav>
         <article class="single-post__main-content">
@@ -72,37 +86,58 @@ if (url.includes("post.html")) {
               <span class="button__title"> چت </span>
             </button>
             <button class="single-post__action-bookmark  button button--icon">
-              <img src="${require("./icons/svgs/bookmark-o.svg" ).default}"/>
+              <img src="${require("./icons/svgs/bookmark-o.svg").default}"/>
             </button>
             <button class="single-post__action-share button button--icon">
-              <img src="${require( "./icons/svgs/share-variant-o.svg").default}"/>
+              <img src="${
+                require("./icons/svgs/share-variant-o.svg").default
+              }"/>
             </button>
           </section>
-          ${infobox!==null?`<section class="single-post__info-box-container">
-          ${infobox.map((item)=>`
+          ${
+            infobox !== null
+              ? `<section class="single-post__info-box-container">
+          ${infobox
+            .map(
+              (item) => `
           <section class="single-post__info-box">
             <span class="single-post__info-box-title"> ${item.title} </span>
             <span class="single-post__info-box-value"> ${item.value} </span>
-          </section>`).join(" ")}
-          </section>`:""}
+          </section>`
+            )
+            .join(" ")}
+          </section>`
+              : ""
+          }
           <hr class="single-post__info-row-separator"/>
           <section class="single-post__info-row-container">
-          ${rows.map((item)=>`
+          ${rows
+            .map(
+              (item) => `
           <section class="single-post__info-row">
             <span class="single-post__info-row-title">${item.data.title}</span>
             <span class="single-post__info-row-value"
               >${item.data.value}</span
             >
           </section>
-          <hr class="single-post__info-row-separator"/>`).join(" ")}
+          <hr class="single-post__info-row-separator"/>`
+            )
+            .join(" ")}
           </section>
           <section class="single-post__description">
             <section class="single-post__description-title">توضیحات</section>
 
-            <section class="single-post__description-text">${description.replace("\\n","<br/>")}</section>
+            <section class="single-post__description-text">${description.replace(
+              "\\n",
+              "<br/>"
+            )}</section>
           </section>
           <section class="single-post__tag-box">
-          ${chips.map((chip) => `<button class="single-post__tag">${chip.text}</button>`).join(" ")}
+          ${chips
+            .map(
+              (chip) => `<button class="single-post__tag">${chip.text}</button>`
+            )
+            .join(" ")}
             
           </section>
         </section>
@@ -110,14 +145,21 @@ if (url.includes("post.html")) {
           <section class="single-post__image-container">
             <img
               class="single-post__image"
-              src="http://placekitten.com/500/500"
+              src="${images[0].image.url}"
             />
           </section>
           <section class="single-post__thumbnail-container">
-            <img
-              class="single-post__thumbnail"
-              src="http://placekitten.com/500/500"
-            />
+            ${images
+              .map(
+                (image) => `
+            <div class="single-post__thumbnail">
+              <img
+                class="single-post__thumbnail-image"
+                src="${image.image.url}"
+              />
+            </div>`
+              )
+              .join(" ")}
           </section>
           <section class="single-post__note-container">
             <textarea id="note" class="single-post__note" type="textbox" placeholder="یادداشت">
@@ -134,6 +176,17 @@ if (url.includes("post.html")) {
           </button>
         </section>
       </article>
-      </main>`
+      </main>`;
+
+      let res = document.querySelectorAll(".single-post__thumbnail");
+      console.log(res);
+      res.forEach((element) => {
+        element.onclick = function (event) {
+          const image = document.querySelector(".single-post__image");
+          const newImage = event.target.src;
+          console.log(newImage);
+          image.src = newImage;
+        };
+      });
     });
 }
